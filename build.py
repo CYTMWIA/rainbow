55391ea4287904eef475d2c12950b2d3141407b6
CYTMWIA <cytmwia@gmail.com>
2023-01-17 20:26:23 +0800

    get time from markdown front matter; add autopep8

--- build.py
+++ build.py
@@ -1,11 +1,17 @@
 #! /usr/bin/python3
 
-################################################################################
-# Settings
-################################################################################
+import json
+import os
+import shutil
+from collections import namedtuple
+from functools import partial
+
+import jinja2
+import markdown
+import md4mathjax
 
 BLOG_TITLE = "Simple Blog"
-TIME_OFFSET = 8 # +0800
+TIME_OFFSET = 8  # +0800
 # https://docs.python.org/zh-cn/3/library/time.html#time.strftime
 TIME_FORMAT = "%y/%m/%d %H:%M:%S"
 MATHJAX_SRC = "https://unpkg.com/mathjax@3.2.0/es5/tex-mml-chtml.js"
@@ -14,66 +20,79 @@ OUTPUT_DIR = "./output"
 CONTENT_DIR = "./content"
 TEMPLATES_DIR = "./templates"
 
-################################################################################
-# Sources
-################################################################################
 
-import datetime
-import os
-import shutil
-import subprocess
-from collections import namedtuple
-from functools import partial
+Article = namedtuple("Article", [
+    "title",
+    "pub_time",
+    "mod_time",
+    "html",
+    "path",
+    "link"
+])
 
-import jinja2
-import markdown
-import md4mathjax
-
-Article = namedtuple("Article", ["title", "time", "html", "link"])
-FileTime = namedtuple("FileTime", ["ctime", "mtime"])
 
 open = partial(open, encoding="utf-8")
 
+
 def ls(path):
-    return [os.path.join(path, p)  for p in  os.listdir(path)]
+    return [os.path.join(path, p) for p in os.listdir(path)]
 
-def format_timestamp(utc_ts):
-    tz = datetime.timezone(datetime.timedelta(hours=TIME_OFFSET))
-    dt = datetime.datetime.fromtimestamp(utc_ts, tz)
-    return dt.strftime(TIME_FORMAT)
 
-def get_filetime_from_git(path: str):
-    args = ["git", "log", "--pretty=%at", "--", path]
-    proc = subprocess.run(args, stdout=subprocess.PIPE)
-    if len(proc.stdout)==0:
-        return FileTime(format_timestamp(0), format_timestamp(0))
-    out = proc.stdout.decode()
-    commits = filter(len, map(lambda s: s.strip(), out.split("\n")))
-    commits = sorted(list(map(int, commits)))
-    return FileTime(format_timestamp(commits[0]), format_timestamp(commits[-1]))
+def take_front_matter(raw_text: str):
+    if raw_text.startswith("{"):
+        split_point, stack, flag_string = None, 0, ""
+        for i, c in enumerate(raw_text):
+            if len(flag_string):
+                if c == flag_string:
+                    flag_string = ""
+            else:
+                if c == "{":
+                    stack += 1
+                elif c == "}":
+                    stack -= 1
+                elif c in "'\"":
+                    flag_string = c
+                if stack == 0:
+                    split_point = i+1
+                    break
+        if split_point:
+            front = json.loads(raw_text[:split_point])
+            text = raw_text[split_point:].lstrip("\n")
+            return front, text
+    return {}, raw_text
+
 
 def build_markdown(path):
-    title = os.path.basename(path)
-    time = get_filetime_from_git(path)
     with open(path, "r") as f:
         raw = f.read()
-    html = markdown.markdown(raw, extensions=[
+    front, text = take_front_matter(raw)
+    html = markdown.markdown(text, extensions=[
         # https://python-markdown.github.io/extensions/
         "markdown.extensions.extra",
         "markdown.extensions.toc",
         "markdown_del_ins",
         md4mathjax.makeExtension(mathjax_src=MATHJAX_SRC)
     ])
-    return Article(title, time, html, None)
+    return Article(
+        front.get("title", os.path.basename(path)),
+        front.get("pub_time", None),
+        front.get("mod_time", None),
+        html,
+        path,
+        None
+    )
+
 
 def load_template(path):
     with open(os.path.join(TEMPLATES_DIR, path), "r") as f:
         return jinja2.Template(f.read())
 
+
 def output_file(path, content):
     p = os.path.join(OUTPUT_DIR, path)
-    with open(p ,"w") as f:
+    with open(p, "w") as f:
         f.write(content)
+
 
 def main():
     article_paths = ls(CONTENT_DIR)
@@ -82,7 +101,7 @@ def main():
     copyfiles = []
     for ap in article_paths:
         filename = os.path.basename(ap)
-        if filename=="about.md":
+        if filename == "about.md":
             about = build_markdown(ap)
         elif filename.startswith("."):
             pass
@@ -90,8 +109,12 @@ def main():
             articles.append(build_markdown(ap))
         else:
             copyfiles.append(ap)
-    articles.sort(key=lambda a:a.time.ctime, reverse=True)
-    articles = list(map(lambda a: a._replace(link=a.title+".html"), articles))
+    articles.sort(key=lambda a: a.pub_time, reverse=True)
+
+    articles = list(map(
+        lambda a: a._replace(link=os.path.basename(a.path)+".html"),
+        articles
+    ))
 
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     output_file("index.html", load_template("index.html").render(
@@ -115,5 +138,5 @@ def main():
             shutil.copy(cp, OUTPUT_DIR)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
