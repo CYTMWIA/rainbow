f1994058bb47552a7390f41a5642ab68d7c18bf9
CYTMWIA <cytmwia@gmail.com>
2023-05-09 19:43:26 +0800

    Do not build frontend when it was built before

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,9 +5,6 @@ set -e
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 ROOT=$(dirname -- "${SCRIPT_DIR}")
 
-if [ ! -d "${ROOT}/frontend/dist" ]; then
-    ${ROOT}/frontend/scripts/build.sh
-fi
+rm -rf "${ROOT}/frontend/dist"
 
-source ${ROOT}/venv/bin/activate
-python ${ROOT}/tools/build.py
+"${ROOT}/scripts/build.sh"
