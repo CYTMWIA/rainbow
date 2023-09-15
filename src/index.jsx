251afb929ebcdc61b12abf43a1cdfee9f2932666
CYTMWIA <cytmwia@gmail.com>
2023-09-16 03:14:42 +0800

    random background color

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('content'));
+function random_background_color() {
+    let h = (Math.random() * 360).toString() + 'deg'
+    let s = (Math.random() * 100) + '%'
+    let l = (80 + Math.random() * 15) + '%'
+    document.body.style.backgroundColor = `hsl(${h}, ${s}, ${l})`
+}
+
+random_background_color()
+const root = createRoot(document.getElementById('content'))
 root.render(
     <h1>Hello World</h1>
-);
+)
