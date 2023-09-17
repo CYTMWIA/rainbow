b46ed65134ad67c18fd0b8581295aa821f5103ff
CYTMWIA <cytmwia@gmail.com>
2023-09-17 14:19:20 +0800

    optional time in articles.tsx

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -13,11 +13,15 @@ function Content() {
         })
     }, [query.manifest]);
 
-    articles.sort((a, b) => b.pub_time - a.pub_time) // 按发布时间降序
+    articles.sort((a, b) => {
+        let at = a.pub_time ? a.pub_time : 0
+        let bt = b.pub_time ? b.pub_time : 0
+        return bt - at
+    }) // 按发布时间降序
     const list = articles.map((art) => {
         return <li key={art.manifest}>
             <a href={`index.html?${art.manifest}`}>{art.title}</a>
-            <div>{format_time(art.pub_time)}</div>
+            <div>{art.pub_time ? format_time(art.pub_time) : ''}</div>
         </li>
     })
     return <>
